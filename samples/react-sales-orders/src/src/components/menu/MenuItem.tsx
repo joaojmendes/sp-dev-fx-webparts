ce527674dac135a05b2f53663ea6dc3f45884350
João Mendes <joao.j.mendes@outlook.com>
2024-05-03 00:54:06 +0100

    commit

--- samples/react-sales-orders/src/src/components/menu/MenuItem.tsx
+++ samples/react-sales-orders/src/src/components/menu/MenuItem.tsx
@@ -1,15 +1,10 @@
-import * as React from 'react';
+import * as React from "react";
 
-import {
-  Body1Strong,
-  Caption1,
-  mergeClasses,
-  tokens,
-} from '@fluentui/react-components';
-import { Icon } from '@iconify/react';
+import { Body1Strong, Caption1, mergeClasses, tokens } from "@fluentui/react-components";
+import { Icon } from "@iconify/react";
 
-import { IMenuItem } from '../../models/IMenuItem';
-import { useMenuStyles } from './useMenuStyles';
+import { IMenuItem } from "../../models/IMenuItem";
+import { useMenuStyles } from "./useMenuStyles";
 
 export interface IStepItemProps {
   item: IMenuItem;
@@ -18,32 +13,34 @@ export interface IStepItemProps {
 }
 
 export const MenuItem: React.FunctionComponent<IStepItemProps> = (props: React.PropsWithChildren<IStepItemProps>) => {
-  const { item, isCurrentItem,   onItemClick } = props;
+  const { item, isCurrentItem, onItemClick } = props;
   const { icon, title, description } = item;
   const styles = useMenuStyles();
-  
-  const color = isCurrentItem ? tokens.colorBrandForeground1 : tokens.colorNeutralStrokeAccessible;
-   
- 
 
-  const ItemInfo = (props: { title: string; description: string; icon: JSX.Element;  }):JSX.Element => {
+  const color = isCurrentItem ? tokens.colorBrandForeground1 : tokens.colorNeutralStrokeAccessible;
+
+  const ItemInfo = (props: { title: string; description: string; icon: JSX.Element }): JSX.Element => {
     const { title, description, icon } = props;
     return (
-        <div className={styles.itemContainer}>
-            <div className={styles.itemTitle}>
-             {icon}
-            <Body1Strong className={styles.headerTitle}  style={{ color: color   }}>{title}</Body1Strong>
-            </div>
-            <Caption1 className={mergeClasses(styles.headerDescription, styles.itemDescription)}>
-             {description}
-            </Caption1>
+      <div className={styles.itemContainer}>
+        <div className={styles.itemTitle}>
+          {icon}
+          <Body1Strong className={styles.headerTitle} style={{ color: color }}>
+            {title}
+          </Body1Strong>
         </div>
-    )
+        <Caption1 className={mergeClasses(styles.headerDescription, styles.itemDescription)}>{description}</Caption1>
+      </div>
+    );
   };
   return (
     <>
-      <div  style={{color: color}}  className={mergeClasses(styles.menuItem)} onClick={() => onItemClick()}>
-        <ItemInfo title={title} description={description as string} icon={<Icon icon={`${icon}`} width={24} height={24}  color={color}/> } />
+      <div style={{ color: color }} className={mergeClasses(styles.menuItem)} onClick={() => onItemClick()}>
+        <ItemInfo
+          title={title}
+          description={description as string}
+          icon={<Icon icon={`${icon}`} width={24} height={24} color={color} />}
+        />
       </div>
     </>
   );
